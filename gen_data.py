2213e6179725105ab85c8b4d860ffd24a78a75c3
shufflesninja <isaact23@vt.edu>
2022-04-16 10:19:42 -0400

    Finish code to generate travel data

--- gen_data.py
+++ gen_data.py
@@ -1,5 +1,13 @@
 # Generate the travel itineraries from scratch.
 
+import csv, os, random
+
+ROWS = 100
+AIRLINES = 4
+LAST_DEPARTURE = 216  # 6 pm
+
+OUTPUT = "data.csv"
+
 CITIES = [("New York", 40.66, 73.93),
           ("Los Angeles", 34.01, 118.41),
           ("Chicago", 41.83, 87.68),
@@ -21,3 +29,47 @@ CITIES = [("New York", 40.66, 73.93),
           ("Denver", 39.76, 104.88),
           ("Washington D.C.", 38.90, 77.01)]
 
+
+# Estimate distance between cities
+def get_dist(city1, city2):
+    lat1 = CITIES[city1][1]
+    long1 = CITIES[city1][2]
+    lat2 = CITIES[city2][1]
+    long2 = CITIES[city2][2]
+    return (((lat2 - lat1) ** 2) + ((long2 - long1) ** 2)) ** 0.5
+
+# Get random city index
+def rand_city():
+    return random.randint(0, len(CITIES) - 1)
+
+def main():
+    if os.path.exists(OUTPUT):
+        os.remove(OUTPUT)
+    with open(OUTPUT, newline='', mode='w') as f:
+        writer = csv.writer(f)
+        writer.writerow(["Departure City", "Departure Time", "Arrival City", "Arrival Time", "Cost", "Airline"])
+
+        # Generate random entries for travel
+        for i in range(ROWS):
+            dep = rand_city()
+            arr = rand_city()
+            while dep == arr:
+                arr = rand_city()
+            dep_time = random.randint(0, LAST_DEPARTURE)
+            dist = get_dist(dep, arr)
+            cost = round(dist * 3, 2)
+            time = round(dist * 1.5)
+            arr_time = dep_time + time
+
+            # Make sure the flight arrives before midnight
+            if arr_time > 287:
+                dep_time -= arr_time - 287
+                arr_time = 287
+
+            airline = random.randint(0, AIRLINES - 1)
+
+            writer.writerow([dep, dep_time, arr, arr_time, cost, airline])
+
+
+if __name__ == "__main__":
+    main()
