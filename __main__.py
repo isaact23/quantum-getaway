67713f3c1189a6cb90b0c373c3bdc418f867c4c7
shufflesninja <isaact23@vt.edu>
2022-04-16 12:15:35 -0400

    Add code to use Qubo

--- __main__.py
+++ __main__.py
@@ -1,7 +1,16 @@
 # Determine the best way to get from point A to point B, optimizing for cost, travel time or number of flights.
 
 from cities import CITIES
+from qubo import Qubo
 
 DEPARTURE = 0
 ARRIVAL = 1
 
+
+def main():
+    qubo = Qubo(1, 0, 1)
+    qubo.solve()
+    qubo.analyze_results()
+
+if __name__ == "__main__":
+    main()
