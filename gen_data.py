cbece16863cbfe2279d1bd38971b73aa5b4dfc13
shufflesninja <isaact23@vt.edu>
2022-04-16 09:51:50 -0400

    Add top 20 cities with coordinates

---
+++ gen_data.py
@@ -0,0 +1,23 @@
+# Generate the travel itineraries from scratch.
+
+CITIES = [("New York", 40.66, 73.93),
+          ("Los Angeles", 34.01, 118.41),
+          ("Chicago", 41.83, 87.68),
+          ("Houston", 29.78, 95.39),
+          ("Phoenix", 33.57, 112.09),
+          ("Philadelphia", 40.00, 75.13),
+          ("San Antonio", 29.47, 98.52),
+          ("San Diego", 32.81, 117.13),
+          ("Dallas", 32.79, 96.76),
+          ("San Jose", 37.29, 121.81),
+          ("Austin", 30.30, 97.75),
+          ("Jacksonville", 30.33, 81.66),
+          ("Fort Worth", 32.78, 97.34),
+          ("Columbus", 39.98, 82.98),
+          ("Indianapolis", 39.77, 86.14),
+          ("Charlotte", 35.20, 80.83),
+          ("San Francisco", 37.72, 123.03),
+          ("Seattle", 47.62, 122.35),
+          ("Denver", 39.76, 104.88),
+          ("Washington D.C.", 38.90, 77.01)]
+
